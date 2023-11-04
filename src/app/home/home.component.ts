d066b2d66f70a2e324a0b758956f39b5a5d8b6fd
dilminx <pdminipura@gmail.com>
2023-11-04 11:51:03 +0530

    add images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FoodService } from "../services/food/food.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+foods:String[] = [];
+  constructor(private foodService: FoodService){}
+ 
+  ngOnInit(): void {
+    this.foods =this.foodService.getAll();
+  }
 }
--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
+  getAll():String[]{
+    return[
+      '/assets/images/foodimg/p1.png',
+      '/assets/images/foodimg/p2.png',
+      '/assets/images/foodimg/p3.png',
+      '/assets/images/foodimg/p4.png',
+      '/assets/images/foodimg/p5.png',
+      '/assets/images/foodimg/p6.png',
+      '/assets/images/foodimg/p7.png',
+      '/assets/images/foodimg/p8.png',
+      '/assets/images/foodimg/p9.png',
+      '/assets/images/foodimg/p10.png',
+      '/assets/images/foodimg/p11.png',
+      '/assets/images/foodimg/p12.png',
+      '/assets/images/foodimg/p13.png',
+      '/assets/images/foodimg/p14.png',
+    ]
+  }
 }
